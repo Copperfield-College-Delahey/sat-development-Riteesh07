88f1efdad23f6f0676deadd195fbedaa0dad0272
Riteesh07 <riteeshravikumaren@gmail.com>
2025-07-01 11:01:59 +1000

    1/07 added booking box

--- Gui.py
+++ Gui.py
@@ -1,6 +1,10 @@
 import customtkinter as ctk 
 from tkinter import messagebox
 
+# App setup
+ctk.set_appearance_mode("light")
+ctk.set_default_color_theme("blue")
+
 #app
 app = ctk.CTk()
 app.title("Badminton Booking System") 
@@ -33,8 +37,38 @@ userPasswordLabel.pack(anchor="w", padx=20)
 userPassword = ctk.CTkEntry(inputFrame, width=300, show="*")
 userPassword.pack(padx=20, pady=5)
 
-#button frame
-buttonFrame = ctk.CTkFrame(app, fg_color="Black")
+#functions
+def sign_in():
+    name = userName.get()
+    email = userEmail.get()
+    password = userPassword.get()
+
+    if not name or not email or not password:
+        messagebox.showerror("Error", "Please fill in the fields.")
+    else:
+        messagebox.showinfo("Signed In", f"Welcome, {name}!")
+
+
+
+def sign_up():
+    name = userName.get()
+    email = userEmail.get()
+    password = userPassword.get()
+
+    if not name or not email or not password:
+        messagebox.showerror("Error", "Please fill in the fields")
+    else:
+        messagebox.showinfo("Signed Up", f"acoount is created, {name}!")
+
+def showBookingAvailabilities():
+    messagebox.showinfo("Bookings")
+
+def cancelSelectedBooking():
+    messagebox.showinfo("Cancelled", "Booking has been cancelled.")
+
+
+#button frames 
+buttonFrame = ctk.CTkFrame(app, fg_color="black")
 buttonFrame.pack(pady=0)
 
 signInButton = ctk.CTkButton(buttonFrame, font=("Bold",15), text="Sign In", fg_color="Black")
@@ -43,5 +77,23 @@ signInButton.pack(anchor="w", padx=20)
 signUpButton = ctk.CTkButton(buttonFrame, font=("Bold",15), text="Sign Up", fg_color="Black")
 signUpButton.pack(anchor="w", padx=20)
 
+
+#Booking Box
+ctk.CTkLabel(app, text="Your Bookings:", font=ctk.CTkFont(size=15, underline=True)).pack(pady=(20, 0))
+
+bookingDisplay = ctk.CTkTextbox(app, width=600, height=100, fg_color="light blue", text_color="white")
+bookingDisplay.insert("0.0", "Date: 25/07/2025 | Court: 1 | Time: 6:00 PM – 7:00 PM")
+bookingDisplay.configure(state="disabled") #ai
+bookingDisplay.pack(pady=10)
+
+#Green red button
+actionFrame = ctk.CTkFrame(app, fg_color="transparent")
+actionFrame.pack(pady=10)
+
+showButton = ctk.CTkButton(actionFrame, text="Show Booking Availabilities", fg_color="green", command=showBookingAvailabilities, width=260)
+showButton.pack(pady=5)
+
+cancelButton = ctk.CTkButton(actionFrame, text="Cancel Selected Bookings", fg_color="red", command=cancelSelectedBooking, width=260)
+cancelButton.pack(pady=5)
 
 app.mainloop()
